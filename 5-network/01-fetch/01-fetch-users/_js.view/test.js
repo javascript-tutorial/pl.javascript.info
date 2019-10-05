d9a9d017bdda357d66c50c25eb7deb52ccb15eb2
mitroc <mckltlo@gmail.com>
2019-10-05 20:35:22 +0200

    Translate 'test.js'

--- 5-network/01-fetch/01-fetch-users/_js.view/test.js
+++ 5-network/01-fetch/01-fetch-users/_js.view/test.js
@@ -1,7 +1,7 @@
 describe("getUsers", function() {
 
-  it("gets users from GitHub", async function() {
-    let users = await getUsers(['iliakan', 'remy', 'no.such.users']);
+  it("pobiera użytkowników GitHuba", async function() {
+    let users = await getUsers(['iliakan', 'remy', 'brak.użytkowników']);
     assert.equal(users[0].login, 'iliakan');
     assert.equal(users[1].login, 'remy');
     assert.equal(users[2], null);
